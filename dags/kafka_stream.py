58ea70c3c06ae36cf5cca5409d94718045baac06
baccouchee <omar.baccouche.10@gmail.com>
2024-10-29 17:40:56 +0100

    init airflow

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 
 default_args = {
     'owner': 'airscholar',
-    'start_date': datetime(2024, 10, 29, 10, 00)
+    'start_date': datetime(2024, 10, 28, 13, 38)
 }
 
 def get_data():
@@ -40,35 +40,31 @@ def stream_data():
     import json
     from kafka import KafkaProducer
     import time
-    # import logging
+    import logging
 
-    res = get_data()
-    res = format_data(res)
-    print(json.dumps(res,indent=3))
+ 
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    curr_time = time.time()
+    while True:
+        if time.time() > curr_time + 60: #1 minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
 
-    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
-    # curr_time = time.time()
-    producer.send('users_created', json.dumps(res).encode('utf-8'))
-    # while True:
-    #     if time.time() > curr_time + 60: #1 minute
-    #         break
-    #     try:
-    #         res = get_data()
-    #         res = format_data(res)
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
 
-    #         producer.send('users_created', json.dumps(res).encode('utf-8'))
-    #     except Exception as e:
-    #         logging.error(f'An error occured: {e}')
-    #         continue
+with DAG('user_automation',
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dag:
 
-# with DAG('user_automation',
-#          default_args=default_args,
-#          schedule_interval='@daily',
-#          catchup=False) as dag:
+    streaming_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_data
+    )
 
-#     streaming_task = PythonOperator(
-#         task_id='stream_data_from_api',
-#         python_callable=stream_data
-#     )
-
-stream_data()
+# stream_data()
